c8d289ee329b7067bc867f5987c8579cc157b111
Tnixc <tnixxc@gmail.com>
2023-12-13 17:10:15 -0500

    stufff

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,18 +1,352 @@
 <template>
-  <div class="h-20"></div>
+  <div class="flex flex-row-reverse justify-around overflow-hidden">
+    <svg
+      width="200"
+      height="200"
+      viewBox="0 0 200 200"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_238_1190)">
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M99.9976 0H38C17.0132 0 0 17.0132 0 38V162C0 182.987 17.0132 200 38 200H99.9976C99.9992 199.834 100 199.667 100 199.5C100 199.667 100.001 199.834 100.002 200H162C182.987 200 200 182.987 200 162V38C200 17.0132 182.987 0 162 0H100.002C100.271 27.6601 122.776 50 150.5 50H150C122.386 50 100 72.3858 100 100C100 72.3858 77.6142 50 50 50H49.5C77.2235 50 99.7293 27.6601 99.9976 0ZM100 100C100 125.286 81.2299 146.188 56.8629 149.533C81.2634 153.097 100 174.111 100 199.5C100 174.111 118.737 153.097 143.137 149.533C118.77 146.188 100 125.286 100 100Z"
+          fill="url(#paint0_linear_238_1190)"
+        />
+      </g>
+      <defs>
+        <linearGradient
+          id="paint0_linear_238_1190"
+          x1="27.5"
+          y1="19"
+          x2="149"
+          y2="174.5"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#FFD9A0" />
+          <stop offset="1" stop-color="#FFF5F1" />
+        </linearGradient>
+        <clipPath id="clip0_238_1190">
+          <rect width="200" height="200" fill="white" />
+        </clipPath>
+      </defs></svg
+    ><svg
+      width="200"
+      height="200"
+      viewBox="0 0 200 200"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_234_920)">
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M0 100L4.37114e-06 0L100 4.37114e-06C100 55.2285 55.2285 100 0 100ZM200 100C200 44.7716 155.228 1.88558e-05 100 4.37114e-06L100 100L6.5969e-06 100C1.76868e-06 155.228 44.7715 200 100 200H200L200 100ZM199.961 100L100 100L100 200C100 144.785 144.75 100.021 199.961 100Z"
+          fill="url(#paint0_linear_234_920)"
+        />
+      </g>
+      <defs>
+        <linearGradient
+          id="paint0_linear_234_920"
+          x1="157.5"
+          y1="32"
+          x2="44"
+          y2="147.5"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset="0.0509862" stop-color="#FFB6E1" />
+          <stop offset="1" stop-color="#FBE3EA" />
+        </linearGradient>
+        <clipPath id="clip0_234_920">
+          <rect width="200" height="200" fill="white" />
+        </clipPath>
+      </defs></svg
+    ><svg
+      width="200"
+      height="200"
+      viewBox="0 0 200 200"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_235_970)">
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M0 100L7.62939e-06 0H100H200V100C144.78 100 100.013 55.2417 100 0.0239258C99.987 55.2417 55.2204 100 0 100ZM100 200C100 144.771 144.772 100 200 100V200H100ZM100 200C100 144.771 55.2285 100 0 100V200H100Z"
+          fill="url(#paint0_linear_235_970)"
+        />
+      </g>
+      <defs>
+        <linearGradient
+          id="paint0_linear_235_970"
+          x1="20.5"
+          y1="16"
+          x2="100"
+          y2="200"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#ACAAFF" />
+          <stop offset="1" stop-color="#C0E8FF" />
+        </linearGradient>
+        <clipPath id="clip0_235_970">
+          <rect width="200" height="200" fill="white" />
+        </clipPath>
+      </defs></svg
+    ><svg
+      width="200"
+      height="200"
+      viewBox="0 0 200 200"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_234_877)">
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M100 100H0V0H7.04252e-06C55.2285 4.82823e-06 100 44.7715 100 100ZM200 100V0C144.772 0 100 44.7715 100 100L200 100ZM100 200H0V100C55.2285 100 100 144.772 100 200ZM100 200H200V100C144.772 100 100 144.772 100 200Z"
+          fill="url(#paint0_linear_234_877)"
+        />
+      </g>
+      <defs>
+        <linearGradient
+          id="paint0_linear_234_877"
+          x1="100"
+          y1="0"
+          x2="100"
+          y2="200"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#B8DBFC" />
+          <stop offset="1" stop-color="#F8FBFE" />
+        </linearGradient>
+        <clipPath id="clip0_234_877">
+          <rect width="200" height="200" fill="white" />
+        </clipPath>
+      </defs></svg
+    ><svg
+      width="200"
+      height="200"
+      viewBox="0 0 200 200"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_236_1011)">
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M0 200L1.63717e-05 100L2.18557e-06 -1.52588e-05L100 -1.96299e-05L200 -8.74228e-06L200 100L200 200L100 200L0 200ZM100 149.973C100.015 122.371 122.395 100 150 100C122.386 100 100 77.6142 100 50C100 77.6142 77.6142 100 50 100C77.6052 100 99.9854 122.371 100 149.973ZM50 126C63.2548 126 74 136.745 74 150C74 163.255 63.2548 174 50 174C36.7452 174 26 163.255 26 150C26 136.745 36.7452 126 50 126ZM74 50C74 63.2548 63.2548 74 50 74C36.7452 74 26 63.2548 26 50C26 36.7451 36.7452 26 50 26C63.2548 26 74 36.7451 74 50ZM126 150C126 136.745 136.745 126 150 126C163.255 126 174 136.745 174 150C174 163.255 163.255 174 150 174C136.745 174 126 163.255 126 150ZM126 50C126 63.2548 136.745 74 150 74C163.255 74 174 63.2548 174 50C174 36.7452 163.255 26 150 26C136.745 26 126 36.7451 126 50Z"
+          fill="url(#paint0_linear_236_1011)"
+        />
+      </g>
+      <defs>
+        <linearGradient
+          id="paint0_linear_236_1011"
+          x1="-4.37114e-06"
+          y1="100"
+          x2="200"
+          y2="100"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#DF99F7" />
+          <stop offset="1" stop-color="#FFDBB0" />
+        </linearGradient>
+        <clipPath id="clip0_236_1011">
+          <rect
+            width="200"
+            height="200"
+            fill="white"
+            transform="translate(0 200) rotate(-90)"
+          />
+        </clipPath>
+      </defs></svg
+    ><svg
+      width="200"
+      height="200"
+      viewBox="0 0 200 200"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_221_21)">
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M0 0H100C44.7715 0 0 44.7715 0 100V0ZM100 200H0V100C0 155.228 44.7715 200 100 200ZM200 100V200H100C155.228 200 200 155.228 200 100ZM200 100V0H100C155.228 0 200 44.7715 200 100ZM100 111C106.075 111 111 106.075 111 100C111 93.9249 106.075 89 100 89C93.9249 89 89 93.9249 89 100C89 106.075 93.9249 111 100 111Z"
+          fill="url(#paint0_linear_221_21)"
+        />
+      </g>
+      <defs>
+        <linearGradient
+          id="paint0_linear_221_21"
+          x1="100"
+          y1="0"
+          x2="100"
+          y2="200"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#DF99F7" />
+          <stop offset="1" stop-color="#FFDBB0" />
+        </linearGradient>
+        <clipPath id="clip0_221_21">
+          <rect width="200" height="200" fill="white" />
+        </clipPath>
+      </defs></svg
+    ><svg
+      width="200"
+      height="200"
+      viewBox="0 0 200 200"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_231_793)">
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M50 0H200V50V150L150 200L150 50H0L50 0ZM0 165.067V100L65.067 100L0 165.067ZM100 200H35.7777L100 135.778L100 200Z"
+          fill="url(#paint0_linear_231_793)"
+        />
+      </g>
+      <defs>
+        <linearGradient
+          id="paint0_linear_231_793"
+          x1="177"
+          y1="-9.23648e-06"
+          x2="39.5"
+          y2="152.5"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#B0B9FF" />
+          <stop offset="1" stop-color="#E7E9FF" />
+        </linearGradient>
+        <clipPath id="clip0_231_793">
+          <rect width="200" height="200" fill="white" />
+        </clipPath>
+      </defs></svg
+    ><svg
+      width="200"
+      height="200"
+      viewBox="0 0 200 200"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_227_4)">
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M100 200C155.228 200 200 155.228 200 100C200 44.7715 155.228 0 100 0C44.7715 0 0 44.7715 0 100C0 155.228 44.7715 200 100 200ZM100 188C148.601 188 188 148.601 188 100C188 51.3989 148.601 12 100 12C51.3989 12 12 51.3989 12 100C12 148.601 51.3989 188 100 188ZM100 176C141.974 176 176 141.974 176 100C176 58.0264 141.974 24 100 24C58.0264 24 24 58.0264 24 100C24 141.974 58.0264 176 100 176ZM100 164C135.346 164 164 135.346 164 100C164 64.6538 135.346 36 100 36C64.6538 36 36 64.6538 36 100C36 135.346 64.6538 164 100 164ZM152 100C152 128.719 128.719 152 100 152C71.2812 152 48 128.719 48 100C48 71.2812 71.2812 48 100 48C128.719 48 152 71.2812 152 100ZM140 100C140 122.091 122.091 140 100 140C77.9086 140 60 122.091 60 100C60 77.9086 77.9086 60 100 60C122.091 60 140 77.9086 140 100Z"
+          fill="url(#paint0_linear_227_4)"
+        />
+      </g>
+      <defs>
+        <linearGradient
+          id="paint0_linear_227_4"
+          x1="27.5"
+          y1="19"
+          x2="149"
+          y2="174.5"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#FFD9A0" />
+          <stop offset="1" stop-color="#FFF5F1" />
+        </linearGradient>
+        <clipPath id="clip0_227_4">
+          <rect width="200" height="200" fill="white" />
+        </clipPath>
+      </defs></svg
+    ><svg
+      width="200"
+      height="200"
+      viewBox="0 0 200 200"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_104_44)">
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M120 0H80V51.7157L43.4315 15.1472L15.1472 43.4314L51.7158 80H0V120H51.7157L15.1472 156.568L43.4315 184.853L80 148.284V200H120V148.284L156.569 184.853L184.853 156.569L148.284 120H200V80H148.284L184.853 43.4314L156.569 15.1471L120 51.7157V0Z"
+          fill="url(#paint0_linear_104_44)"
+        />
+      </g>
+      <defs>
+        <linearGradient
+          id="paint0_linear_104_44"
+          x1="100"
+          y1="0"
+          x2="100"
+          y2="200"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#A7B5FF" />
+          <stop offset="1" stop-color="#F3ACFF" />
+        </linearGradient>
+        <clipPath id="clip0_104_44">
+          <rect width="200" height="200" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+    <svg width="200" height="200" viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg"> <g clip-path="url(#clip0_105_300)"> <path fill-rule="evenodd" clip-rule="evenodd" d="M188.941 100C192.769 100 196.477 99.4861 200 98.5234C197.199 97.9327 194.295 97.622 191.319 97.622H141.855C133.685 97.622 126.061 95.2811 119.619 91.2334C121.027 82.9896 124.913 75.0866 131.277 68.7229L162.891 37.1094C165.598 34.4024 167.856 31.4169 169.667 28.2452C167.268 29.8079 164.995 31.6416 162.891 33.7463L127.914 68.7229C122.137 74.4999 115.091 78.2351 107.674 79.9285C102.84 73.1033 100 64.7671 100 55.7675V11.0593C100 7.23107 99.4861 3.5229 98.5234 0C97.9327 2.80071 97.622 5.70466 97.622 8.68118L97.622 58.1456C97.622 66.3155 95.2811 73.9388 91.2335 80.3812C82.9897 78.9727 75.0866 75.0865 68.7229 68.7228L37.1094 37.1094C34.4024 34.4024 31.4169 32.1437 28.2452 30.3334C29.8079 32.7315 31.6416 35.0047 33.7463 37.1094L68.7229 72.086C74.4999 77.8631 78.2352 84.9088 79.9285 92.3263C73.1033 97.1596 64.7671 100 55.7675 100H11.0593C7.23105 100 3.52289 100.514 0 101.477C2.80073 102.067 5.70469 102.378 8.68122 102.378H58.1456C66.3156 102.378 73.9388 104.719 80.3812 108.767C78.9727 117.01 75.0866 124.913 68.7229 131.277L37.1094 162.891C34.4025 165.598 32.1438 168.583 30.3335 171.755C32.7316 170.192 35.0047 168.358 37.1094 166.254L72.086 131.277C77.863 125.5 84.9088 121.765 92.3263 120.071C97.1596 126.897 100 135.233 100 144.233V188.941C100 192.769 100.514 196.477 101.477 200C102.067 197.199 102.378 194.295 102.378 191.319V141.854C102.378 133.684 104.719 126.061 108.767 119.619C117.01 121.027 124.913 124.913 131.277 131.277L162.891 162.891C165.598 165.598 168.583 167.856 171.755 169.667C170.192 167.268 168.358 164.995 166.254 162.891L131.277 127.914C125.5 122.137 121.765 115.091 120.072 107.674C126.897 102.84 135.233 100 144.233 100H188.941Z" fill="url(#paint0_linear_105_300)"/> </g> <defs> <linearGradient id="paint0_linear_105_300" x1="100" y1="0" x2="100" y2="200" gradientUnits="userSpaceOnUse"> <stop stop-color="#B8DBFC"/> <stop offset="1" stop-color="#F8FBFE"/> </linearGradient> <clipPath id="clip0_105_300"> <rect width="200" height="200" fill="white"/> </clipPath> </defs> </svg>
+  </div>
   <div class="p-8 text-rp-subtle lg:p-20">
-  <div class="flex justify-between">
-  <div>
-    <p>Made with the <a href="https://rosepinetheme.com/" target="_blank" class="text-rp-pine transition-all hover:text-rp-foam">rose pine color scheme</a>.</p>
-    <p>Made with Vue + Tailwind + Nuxt.</p>
-  </div>
-  <div class="text-right">
-    <p><a href="https://rosepinetheme.com/" target="_blank" class="text-rp-pine transition-all hover:text-rp-foam">Source code on github</a></p>
-    <p>Last updated on {{date}}</p>
-  </div>
-  </div>
+    <div class="flex justify-between">
+      <div>
+        <p>
+          Made with the
+          <a
+            href="https://rosepinetheme.com/"
+            target="_blank"
+            class="text-rp-pine transition-all hover:text-rp-foam"
+            >rose pine color scheme</a
+          >.
+        </p>
+        <p>Made with Vue + Tailwind + Nuxt.</p>
+      </div>
+      <div class="text-right">
+        <p>
+          <a
+            href="https://rosepinetheme.com/"
+            target="_blank"
+            class="text-rp-pine transition-all hover:text-rp-foam"
+            >Source code on github</a
+          >
+        </p>
+        <p>Last updated on {{ d }}</p>
+      </div>
+    </div>
   </div>
 </template>
-<script>
-
+<script lang="ts">
+export default {
+  data() {
+    return {
+      d: null as string | null, // Use null as the initial type
+    };
+  },
+  async mounted() {
+    this.d = await this.getUpdated();
+  },
+  methods: {
+    async getUpdated() {
+      const data = await fetch(
+        "https://api.github.com/repos/Tnixc/website-v3/branches/main",
+      );
+      const json = await data.json();
+      const x = json.commit.commit.author.date;
+      const d = new Date(x);
+      return d.toLocaleDateString();
+    },
+  },
+};
 </script>
+<style scoped>
+svg {
+  padding: 3rem;
+  transition: all 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+}
+svg:hover {
+  transform: rotate(36deg);
+}
+svg:hover ~ svg {
+  transform: rotate(36deg);
+}
+</style>
