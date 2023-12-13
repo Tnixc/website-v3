33fee6e478a9f0ffb86f55ca506b89ce0e40ae70
Tnixc <tnixxc@gmail.com>
2023-12-13 18:19:38 -0500

    Add SEO meta tags and set HTML language attribute

--- app.vue
+++ app.vue
@@ -1,3 +1,28 @@
+<script>useSeoMeta({
+  title: '[title]',
+  description: '[description]',
+  ogTitle: '[og:title]',
+  ogDescription: '[og:description]',
+  ogImage: '[og:image]',
+  ogUrl: '[og:url]',
+  twitterTitle: '[twitter:title]',
+  twitterDescription: '[twitter:description]',
+  twitterImage: '[twitter:image]',
+  twitterCard: 'summary'
+})
+
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.png'
+    }
+  ]
+})</script>
 <template>
   <body class="flex h-screen justify-center bg-black">
     <main class="w-[98vw] max-w-7xl">
