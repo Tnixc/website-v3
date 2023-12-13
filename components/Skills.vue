e00a341ce4753d585e7a295af931161a4770efad
Tnixc <tnixxc@gmail.com>
2023-12-13 12:40:27 -0500

    Add languages section to Skills.vue

--- components/Skills.vue
+++ components/Skills.vue
@@ -38,8 +38,92 @@
       <p class="py-4 font-sans text-xl leading-7 text-rp-subtle">
         I mainly do UI/UX design and Front-end development, but I would like to
         learn more about how js frameworks and web technologies work under the
-        hood. I also do some 3D art and animation. I'm considering a career in UI/UX design, but I'm not sure yet.
+        hood. I also do some 3D art and animation. I'm considering a career in
+        UI/UX design, but I'm not sure yet.
       </p>
+    </div>
+  </div>
+  <div>
+    <div class="w-full rounded-t-6xl bg-rp-base">
+      <div class="px-20 pb-20 pt-20 font-serif text-rp-text">
+        <h1 class="flex gap-4 text-5xl">
+          <svg
+            width="48"
+            height="48"
+            viewBox="0 0 200 200"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g clip-path="url(#clip0_236_1049)">
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M200 2.62268e-06V60L200 200H140V130.033C139.982 168.678 108.649 200 70 200C31.3401 200 0 168.66 0 130C0 91.3401 31.3401 60 70 60C108.649 60 139.982 91.3222 140 129.967V60H70H0V2.62268e-06L140 0L200 2.62268e-06Z"
+                fill="url(#paint0_linear_236_1049)"
+              />
+            </g>
+            <defs>
+              <linearGradient
+                id="paint0_linear_236_1049"
+                x1="157.5"
+                y1="32"
+                x2="44"
+                y2="147.5"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop offset="0.0509862" stop-color="#FFB6E1" />
+                <stop offset="1" stop-color="#FBE3EA" />
+              </linearGradient>
+              <clipPath id="clip0_236_1049">
+                <rect width="200" height="200" fill="white" />
+              </clipPath>
+            </defs></svg
+          >Languages
+        </h1>
+        <div class="flex gap-4 py-8 font-mono">
+          <div class="rounded-lg bg-rpm-base p-4 text-xl">TypeScript</div>
+          <div class="rounded-lg bg-rpm-base p-4 text-xl">HTML</div>
+          <div class="rounded-lg bg-rpm-base p-4 text-xl">CSS</div>
+          <div class="rounded-lg bg-rpm-base p-4 text-xl">SASS</div>
+          <div class="rounded-lg bg-rpm-base p-4 text-xl">Python</div>
+        </div>
+      </div>
+      <div class="px-20 pb-20 pt-20 font-serif text-rp-text">
+        <h1 class="flex gap-4 text-5xl">
+          <svg
+            width="48"
+            height="48"
+            viewBox="0 0 200 200"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g clip-path="url(#clip0_236_1049)">
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M200 2.62268e-06V60L200 200H140V130.033C139.982 168.678 108.649 200 70 200C31.3401 200 0 168.66 0 130C0 91.3401 31.3401 60 70 60C108.649 60 139.982 91.3222 140 129.967V60H70H0V2.62268e-06L140 0L200 2.62268e-06Z"
+                fill="url(#paint0_linear_236_1049)"
+              />
+            </g>
+            <defs>
+              <linearGradient
+                id="paint0_linear_236_1049"
+                x1="157.5"
+                y1="32"
+                x2="44"
+                y2="147.5"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop offset="0.0509862" stop-color="#FFB6E1" />
+                <stop offset="1" stop-color="#FBE3EA" />
+              </linearGradient>
+              <clipPath id="clip0_236_1049">
+                <rect width="200" height="200" fill="white" />
+              </clipPath>
+            </defs></svg
+          >Languages
+        </h1>
+      </div>
     </div>
   </div>
 </template>
