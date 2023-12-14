9af9da14861b0ba2ea822d42ac655a50120d7598
Tnixc <tnixxc@gmail.com>
2023-12-13 23:44:08 -0500

    Add lazy loading to Nav.vue image

--- components/Nav.vue
+++ components/Nav.vue
@@ -4,6 +4,7 @@
   >
     <NuxtImg
       draggable="false"
+      loading="lazy"
       class="rounded-full hidden aspect-square h-full max-h-full scale-75 ring-2 ring-rp-love md:block"
       src="https://avatars.githubusercontent.com/u/85466117?v=4"
     />
