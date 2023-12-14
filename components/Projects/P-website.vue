a3ba7edf2279746400d197b5b1add134967e2b4f
Tnixc <tnixxc@gmail.com>
2023-12-13 23:51:01 -0500

    Update website links in P-website.vue

--- components/Projects/P-website.vue
+++ components/Projects/P-website.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="rounded-3xl flex max-w-full flex-col bg-rp-surface p-10">
-    <div>
+
       <h1 class="flex items-center gap-5 font-serif text-4xl text-rp-text">
         This website
       </h1>
@@ -8,20 +8,23 @@
         I have rewritten this website so many goddamn times. this is only v3.
         Want to go back in time? See v1 or v2, they get progressively worse.
       </p>
+      <br class="mt-auto mb-0">
+      <div>
       <a
         href="https://old-v1-tnixc.vercel.app/"
         target="_blank"
         class="inline font-mono text-lg text-rpd-foam transition-all hover:text-rp-foam hover:underline"
         >old-v1-tnixc.vercel.app/</a
       >
+
       <span class="inline-block w-4"></span>
       <span class="select-none text-rp-subtle">|</span>
       <span class="inline-block w-4"></span>
       <a
-        href="https://website-v2-tnixc.vercel.app"
-        target="_blank"
-        class="inline font-mono text-lg text-rpd-foam transition-all hover:text-rp-foam hover:underline"
-        >website-v2-tnixc.vercel.app</a
+      href="https://website-v2-tnixc.vercel.app"
+      target="_blank"
+      class="inline font-mono text-lg text-rpd-foam transition-all hover:text-rp-foam hover:underline"
+      >website-v2-tnixc.vercel.app</a
       >
     </div>
   </div>
