2657524db1a6a6c999031c103e5510abb259252f
Tnixc <tnixxc@gmail.com>
2023-12-13 23:15:13 -0500

    optimize fonts slightly

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,10 +27,6 @@ export default defineNuxtConfig({
           "4xl": "2rem",
           "5xl": "2.5rem",
           "6xl": "3rem",
-          "7xl": "3.5rem",
-          "8xl": "4rem",
-          "9xl": "4.5rem",
-          "10xl": "5rem",
           full: "9999px",
         },
         extend: {
